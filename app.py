a5d12cf0149b76e3adbf12290108e026db1fd061
EoyoeS <2271021509@qq.com>
2023-04-26 13:59:55 +0800

    使用多个open_api_key，以增加每分钟的调用次数

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from concurrent.futures import ThreadPoolExecutor
 import json
 from pprint import pprint, pformat
 import sqlite3
-from itertools import chain
+from itertools import chain, repeat
 from threading import RLock
+from time import sleep
+from queue import Queue
 
 from flask import Flask, request, jsonify, abort
 import requests
@@ -20,17 +22,21 @@ TENANT_ACCESS_TOKEN = ''
 REPLY_URL = 'https://open.feishu.cn/open-apis/im/v1/messages/{}/reply'
 DB_PATH = 'example.db'  # 相对路径
 
-openai.api_key = config['openai_api_key']
+api_keys = config['openai_api_keys']
 openai.proxy = '[::]:7890'
-pool = ThreadPoolExecutor(3)
+pool = ThreadPoolExecutor()
 app = Flask(__name__)
 db_lock = RLock()
+chatgpt_lock = [RLock() for _ in range(3)]
+message_queue = Queue()
 
 
 def color(code: int):
     def warpper(text):
         return '\033[{}m{}\033[0m'.format(code, text)
+
     return warpper
+
 
 red = color(31)
 green = color(32)
@@ -38,7 +44,7 @@ yellow = color(33)
 blue = color(34)
 
 
-def chat_gpt(content, history=None) -> str:
+def chat_gpt(content: str, history: list, api_key: str) -> str:
     # 调用 ChatGPT 接口
     if history is not None:
         questions = ({'role': 'user', 'content': conv[0]} for conv in history)
@@ -47,15 +53,19 @@ def chat_gpt(content, history=None) -> str:
     else:
         messages = []
     messages += [{'role': 'user', 'content': content}]
-    pprint(f'{history=}')
-    print(red(messages))
+    print(blue(messages))
     try:
         completion = openai.ChatCompletion.create(
+            api_key=api_key,
             model="gpt-3.5-turbo",
-            messages=messages
+            messages=messages,
         )
     except openai.error.RateLimitError:
         return '手速太快了，休息一下吧'
+    except Exception as e:
+        with open('error.log', 'a') as f:
+            f.write(pformat(e))
+        return '出错了，我也不知道为什么'
     print(green(pformat(completion)))
     return completion.choices[0].message.content
 
@@ -77,7 +87,9 @@ def reply(message_id, answer):
             return 200
     # token失效或过期，重新获取
     print(yellow('token expired, renewing...'))
-    with requests.post(TOKEN_URL, data={'app_id': APP_ID, 'app_secret': APP_SECRET}) as r:
+    with requests.post(
+        TOKEN_URL, data={'app_id': APP_ID, 'app_secret': APP_SECRET}
+    ) as r:
         TENANT_ACCESS_TOKEN = r.json()['tenant_access_token']
     headers['Authorization'] = 'Bearer ' + TENANT_ACCESS_TOKEN
     with requests.post(url, data=data, headers=headers) as r:
@@ -87,7 +99,7 @@ def reply(message_id, answer):
         else:
             # 重新获取token失败
             print(red('renew failed'))
-    
+
 
 # 替换掉@信息，保留用户的问题
 def get_qustion(message) -> str:
@@ -103,7 +115,9 @@ def get_history(chat_id):
     with db_lock:
         con = sqlite3.connect(DB_PATH)
         cur = con.cursor()
-        cur.execute(f'create table if not exists {chat_id} (question text, answer text)')
+        cur.execute(
+            f'create table if not exists {chat_id} (question text, answer text)'
+        )
         cur.execute(f'select * from {chat_id}')
         history = cur.fetchall()
         con.commit()
@@ -116,7 +130,7 @@ def delete_history(chat_id):
     with db_lock:
         con = sqlite3.connect(DB_PATH)
         cur = con.cursor()
-        cur.execute(f'drop table {chat_id}')
+        cur.execute(f'drop table if exists {chat_id}')
         con.commit()
         con.close()
 
@@ -129,19 +143,35 @@ def insert_new_convestion(chat_id, question, answer):
         cur.execute(f'insert into {chat_id} values (?, ?)', (question, answer))
         con.commit()
         con.close()
-        
+
 
 # 处理消息
 def handle_message(message):
-    chat_id, message_id = message['chat_id'], message['message_id']
-    question = get_qustion(message)
-    if '/clear' in question:
-        delete_history(chat_id)
-        reply(message_id, '上下文已清除')
-    else:
-        answer = chat_gpt(question, get_history(chat_id))
+    try:
+        chat_id, message_id = message['chat_id'], message['message_id']
+        question = get_qustion(message)
+        if '/clear' in question:
+            delete_history(chat_id)
+            reply(message_id, '上下文已清除')
+        elif '@_all' not in question:
+            message_queue.put((question, message_id, chat_id))
+            sleep(0.1)
+            if not message_queue.empty():
+                reply(message_id, '正在思考中...')
+    except Exception as e:
+        with open('error.log', 'a') as f:
+            f.write(pformat(e))
+        print(red(e))
+        reply(message_id, '出错了，我也不知道为什么')
+
+
+def reply_from_chatgpt(api_key: str):
+    while True:
+        question, message_id, chat_id = message_queue.get()
+        answer = chat_gpt(question, get_history(chat_id), api_key)
         insert_new_convestion(chat_id, question, answer)
         reply(message_id, answer)
+        sleep(60)
 
 
 @app.route('/api', methods=['POST'])
@@ -153,12 +183,11 @@ def api():
     if headers.get('token') != VERIFICATION_TOKEN or headers.get('app_id') != APP_ID:
         abort(403)
     message = request_data['event']['message']
-    # handle_message(message)
-    pool.submit(handle_message, message)  # TODO: 由于openai的每分钟3条信息的限制，需要用其他方法改进
-    # ? 这里为了防止chat-GPT太慢了，导致超时，所以咋暂时直接返回ok，https://open.feishu.cn/document/ukTMukTMukTM/uYDNxYjL2QTM24iN0EjN/event-subscription-configure-/encrypt-key-encryption-configuration-case
-    # ? 可以尝试根据event_id来先把回答存下来，再次遇到event_id时直接回答，但可能导致上下文错乱
+    handle_message(message)
     return jsonify({'msg': 'ok'})
 
 
 if __name__ == '__main__':
+    for api_key in chain.from_iterable(repeat(api_key, 3) for api_key in api_keys):
+        pool.submit(reply_from_chatgpt, api_key)
     app.run(host='0.0.0.0', port=8713, debug=True)
